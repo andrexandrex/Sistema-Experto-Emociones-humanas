95798bedb73f0ccf9452a62e67182dc6a3544de6
Medrano-Sebastian <120860617+Medrano-Sebastian@users.noreply.github.com>
2023-04-30 15:53:01 -0500

    Para comenzar tengo esto

--- final_emociones.pl
+++ final_emociones.pl
@@ -129,4 +129,106 @@ deprimido(X) :- salud_mala(Z),member(X,Z),triste(X).%  deprimido(fernando).
 desanimado(X) :- triste(X), deprimido(X),not(feliz_trabajo(X)) . %desanimado(fernando).
 miedo(X) :- vive_solo(Y),member(X,Y),vive_en(X,U), no_es_seguro(L),member(U,L). %miedo(gerardo).
 ansioso(X) :- preocupado(X);(autoestima_baja_esta(X);autoestima_media_esta(X)). %ansioso(cecilia).
-%Reglas extra
+
+
+
+
+
+%COMIENZO
+estado_emocional:-
+    hipotesis(Enfermedad),
+	write('Al parecer esta experimentando'),
+	write(Enfermedad),
+	nl,
+	write('¿ALguna otra consulta?'),
+  	deshacer.
+
+
+
+
+hipotesis(felicidad):- felicidad.
+hipotesis(tristeza):- tristeza.
+hipotesis(aburrimiento):- aburrimiento.
+hipotesis(orgullo):- orgullo.
+hipotesis(preocupación):- preocupación.
+hipotesis(depresión):- depresión.
+hipotesis(miedo):- miedo.
+hipotesis(ansiedad):- ansiedad.
+hipotesis(animación):-animación.
+
+
+felicidad:-
+    verificar(postura_erguida),
+    verificar(movmientos_rapidos),
+    verificar(circulo_social_grande),
+    verificar(trabajo_agradable).
+
+tristeza:-
+    verificar(postura_encorvada),
+    verificar(movimientos_lentos),
+    verificar(circulo_social_chico),
+    verificar(trabajo_estresante).
+    
+aburrimiento:-
+    verificar(postura_encorvada),
+    verificar(movimientos_lentos),
+    verificar(circulo_social_pequeño),
+    verificar(boztezos).
+    
+orgullo:-
+    verificar(postura_erguida),
+    verificar(movimientos_seguros),
+    verificar(trabajo_agradable),
+    verificar(a).
+preocupación:-
+    verificar(postura_encorvada),
+    verificar(movimientos_nerviosos),
+    verificar(no_saludable),
+    verificar(trabajo_estresante).
+depresión:-
+    verificar(postura_encorvada),
+    verificar(movimientos_lentos),
+    verificar(circulo_social_chico),
+    verificar(trabajo_estresante).
+miedo:-
+    verificar(postura_encorvada),
+    verificar(movimientos_tensos),
+    verificar(no_saludable),
+    verificar(trabajo_estresante).
+ansiedad:-
+    verificar(postura_encorvada),
+    verificar(movimientos_nerviosos),
+    verificar(no_saludable),
+    verificar(trabajo_estresante).
+animación:-
+    verificar(postura_erguida),
+    verificar(movimientos_rápidos),
+    verificar(no_saludable),
+    verificar(trabajo_estresante).
+    
+
+    
+preguntar(Pregunta) :-
+    write('¿El individuo presenta estas caracteristicas?:'),
+    write(Pregunta),
+    write('? '),
+    read(Respuesta),
+    nl,
+    ((Respuesta == si) -> assert(si(Pregunta));
+    assert(no(Pregunta)), fail).
+
+
+:- dynamic si/1,no/1.
+/*Como se verifica */
+verificar(S) :-
+(si(S)->true ;
+(no(S)->fail ;
+preguntar(S))).
+
+
+
+    
+%desahacer    
+deshacer :- retract(si(_)),fail.
+deshacer :- retract(no(_)),fail.
+deshacer.
